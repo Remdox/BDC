de853d31a391352c4c90b325b3d078c39b93fd57
svevaturola <sveva.turola@edu.unife.it>
2025-05-05 12:59:30 +0200

    Updated methods

--- src/G13HW2.java
+++ src/G13HW2.java
@@ -1,14 +1,112 @@
 import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
 import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.mllib.clustering.KMeans;
+import org.apache.spark.mllib.clustering.KMeansModel;
+import org.apache.spark.mllib.linalg.Vector;
+import org.apache.spark.mllib.linalg.*;
+import scala.Tuple2;
+import java.util.*;
+import java.lang.*;
+
+class methodsHW2{
+    public static void MRFairLloyd(JavaRDD<String> inputPoints, Integer K, Integer M){
+        JavaRDD<Vector> parsedInputPoints = conversion(inputPoints);
+        KMeansModel clusters = KMeans.train(parsedInputPoints.rdd(), K, 0);
+        Vector[] C = clusters.clusterCenters();
+        JavaRDD<String> inputPointsA = inputPoints.filter(row -> row.endsWith("A"));
+        JavaRDD<Vector> parsedInputPointsA = conversion(inputPointsA);
+        JavaRDD<String> inputPointsB = inputPoints.filter(row -> row.endsWith("B"));
+        JavaRDD<Vector> parsedInputPointsB = conversion(inputPointsB);
+
+        for(int i = 0; i < M; i++){
+            JavaPairRDD<Vector, Iterable<Vector>> clusterPair = parsedInputPoints.mapToPair(point -> {
+                int clusterIndex = clusters.predict(point);
+                Vector centroid = C[clusterIndex];
+                return new Tuple2<>(centroid, point);
+                
+            }).groupByKey();
+
+            Vector[] centersA = {};
+            Vector[] centersB = {};
+
+        }
+
+    }
+
+    public static JavaRDD<Vector> conversion(JavaRDD<String> inputPoints){
+        JavaRDD<Vector> parsedInputPoints = inputPoints.map(row -> {
+            String[] rowArray = row.split(",");
+            double[] values = new double[rowArray.length - 1];
+            for (int i = 0; i < rowArray.length - 1; i++) {
+                values[i] = Double.parseDouble(rowArray[i]);
+            }
+            return Vectors.dense(values);
+        });
+        parsedInputPoints.cache();
+
+        return parsedInputPoints;
+    }
+
+    public static double[] computeVectorX(double fixedA, double fixedB, double[] alpha, double[] beta, double[] ell, int K) {
+        double gamma = 0.5;
+        double[] xDist = new double[K];
+        double fA, fB;
+        double power = 0.5;
+        int T = 10;
+        for (int t=1; t<=T; t++){
+            fA = fixedA;
+            fB = fixedB;
+            power = power/2;
+            for (int i=0; i<K; i++) {
+                double temp = (1-gamma)*beta[i]*ell[i]/(gamma*alpha[i]+(1-gamma)*beta[i]);
+                xDist[i]=temp;
+                fA += alpha[i]*temp*temp;
+                temp=(ell[i]-temp);
+                fB += beta[i]*temp*temp;
+            }
+            if (fA == fB) {break;}
+            gamma = (fA > fB) ? gamma+power : gamma-power;
+        }
+        return xDist;
+    }
+
+    public static double MRComputeStandardObjective(JavaRDD<Vector> parsedInputPoints, Vector[] C){
+        double totalDistance = parsedInputPoints.map( point -> {
+            double minDistance = Double.MAX_VALUE;
+            for (Vector c : C) {
+                double distance = Vectors.sqdist(point, c);
+                if (distance < minDistance) {
+                    minDistance = distance;
+                }
+            }
+            return minDistance;
+        }).reduce(Double::sum);
+        return (1.0 / parsedInputPoints.count()) * totalDistance;
+    }
+
+    public static double MRComputeFairObjective(JavaRDD<String> inputPoints, Vector[] C){
+        JavaRDD<String> inputPointsA = inputPoints.filter(row -> row.endsWith("A"));
+        JavaRDD<Vector> parsedInputPointsA = conversion(inputPointsA);
+        double deltaA = MRComputeStandardObjective(parsedInputPointsA, C);
+
+        JavaRDD<String> inputPointsB = inputPoints.filter(row -> row.endsWith("B"));
+        JavaRDD<Vector> parsedInputPointsB = conversion(inputPointsB);
+        double deltaB = MRComputeStandardObjective(parsedInputPointsB, C);
+
+        return Double.max(deltaA, deltaB);
+    }
+}
 
 public class G13HW2 {
     static void main(String[] args) {
         if (args.length != 4) {
             throw new IllegalArgumentException("USAGE: file_path L K M");
         }
+
         Logger.getLogger("org").setLevel(Level.OFF);
         Logger.getLogger("akka").setLevel(Level.OFF);
         SparkConf conf = new SparkConf(true).setAppName("G13HW1");
@@ -23,5 +121,16 @@ public class G13HW2 {
         // partition the points into L random partitions for the MapReduce algorithm used later in MRPrintStatistics
         JavaRDD<String> inputPoints = sc.textFile(args[0]).repartition(L).cache();
 
+        // as requested, we count the sizes of the two groups
+        long N = inputPoints.count();
+        long NA = inputPoints.filter(row -> row.endsWith("A")).count();
+        long NB = inputPoints.filter(row -> row.endsWith("B")).count();
+        System.out.println("N = " + N + ", NA= " + NA + ", NB = " + NB);
+
+        // as requested, we first convert the strings given as input into Vector points
+        JavaRDD<Vector> parsedInputPoints = methodsHW2.conversion(inputPoints);
+
+        KMeansModel clusters = KMeans.train(parsedInputPoints.rdd(), K, M);
+        Vector[] C_stand = clusters.clusterCenters();
     }
 }
